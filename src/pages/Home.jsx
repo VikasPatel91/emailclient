ef4f4dcb833af303d0af62a4fbe32809abd2e91a
Vikas Patel <112894254+VikasPatel91@users.noreply.github.com>
2025-01-22 21:35:38 +0530

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -7,7 +7,7 @@ const Home = () => {
 
   useEffect(() => {
     const fetchData = async () => {
-      const res = await axios.get("http://localhost:3500/get_template");
+      const res = await axios.get("https://emailserver-1-gprw.onrender.com/");
       setData(res.data);
     };
     fetchData();
